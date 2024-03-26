bd6798af04052363472f8bf6d906b6bb91a3f1a2
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-03-26 09:01:10 -0400

    Git estudantes.

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,20 +1,194 @@
-import './Main.css'
-import Estudante from '../estudante/Estudante'
-function Main(){
-    return(
-        <main>
-            <Estudante 
-                nome="Guilherme Figueiredo Terenciani"
-                descricao='Professor no IFMS-NV'
-                foto='https://github.com/guilhermeterenciani.png'
-                githubLink='https://github.com/guilhermeterenciani'/>
-            <Estudante 
-                nome="Guilherme Figueiredo Terenciani"
-                descricao='Professor no IFMS-NV'
-                foto='https://github.com/guilhermeterenciani.png'
-                githubLink='https://github.com/guilhermeterenciani'/>
-        </main>
-    )
+import "./Main.css";
+import Estudante from "../estudante/Estudante";
+function Main() {
+  return (
+    <main>
+      <Estudante
+        nome="Guilherme Figueiredo Terenciani"
+        descricao="Professor no IFMS-NV"
+        foto="https://github.com/guilhermeterenciani.png"
+        githubLink="https://github.com/guilhermeterenciani"
+      />
+      <Estudante
+        nome="Rafael Tomazini Marani"
+        descricao="Aluno do IFMS-NV"
+        foto="https://github.com/RafaTMZ.png"
+        githubLink="https://github.com/RafaTMZ"
+      />
+      <Estudante
+        nome="Maria Julia Alves "
+        descricao="Estudante do IFMS-NV"
+        foto="https://github.com/maajulia.png"
+        githubLink="https://github.com/maajulia"
+      />
+      <Estudante
+        nome="Julia Ribeiro"
+        descricao="Aluna no IFMS-NV"
+        foto="https://github.com/juliaribeiroxxx.png"
+        githubLink="https://github.com/juliaribeiroxxx"
+      />
+      <Estudante
+        nome="Nicole de Souza Gois"
+        descricao="estudante do ifms-nv"
+        foto="https://github.com/Nicknicole12.png"
+        githubLink="https://github.com/Nicknicole12/1022A-reactt/tree/main"
+      />
+      <Estudante
+        nome="Katiely Fernanda Góis Santos"
+        descricao="Estudante do IFMS-NV"
+        foto="https://github.com/katielizinha.png"
+        githubLink="https://github.com/katielizinha"
+      />
+      <Estudante
+        nome="Caick gabriel"
+        descricao="Aluno ifms"
+        foto="https://avatars.githubusercontent.com/u/109927313?v=4"
+        githubLink="https://github.com/tio-mec"
+      />
+      <Estudante
+        nome="Rafael Miranda da Silva"
+        descricao="Aluno turma 1022-A IFMS-NV"
+        foto="https://github.com/MiraMiranda.png"
+        githubLink="https://github.com/MiraMiranda"
+      />
+
+      <Estudante
+        nome="José Rosa Leite"
+        descricao="Estudante no IFMS"
+        foto="https://github.com/josefh852.png"
+        githubLink="https://github.com/josefh852"
+      />
+      <Estudante
+        nome="Gabriele Pimentel Flores"
+        descricao="Aluna no IFMS-NV"
+        foto="https://github.com/gabiiiflores.png"
+        githubLink="https://github.com/gabiiiflores"
+      />
+      <Estudante
+        nome="Narriane Aparecida Neves Silva"
+        descricao="Aluna do IFMS no 3ano de informatica"
+        foto="https://github.com/narrianesil.png"
+        githubLink="https://github.com/narrianesil"
+      />
+      <Estudante
+        nome="Tainara de Oliveira Dias"
+        descricao="Aluna no IFMS-NV"
+        foto="https://github.com/account"
+        githubLink="https://github.com/"
+      />
+      <Estudante
+        nome="Ryan Gosling"
+        descricao="I drive"
+        foto="https://github.com/pkz1nzz.png"
+        githubLink="https://github.com/pkz1nzz"
+      />
+      <Estudante
+        nome="Clara Elis da Silva Larsen"
+        descricao="Aluna do IFMS-NV"
+        foto="https://github.com/Claraeex.png"
+        githubLink="https://github.com/Claraeex"
+      />
+
+      <Estudante
+        nome="Camila Haiane dos Santos Pereira"
+        descricao="Estudante no IFMS-NV"
+        foto="https://github.com/camilahsp.png"
+        githubLink="https://github.com/camilahsp"
+      />
+      <Estudante
+        nome="Kauã Luiz dos Santos"
+        descricao="Aluno do Instituto Federal de Mato Grosso do Sul"
+        foto="https://github.com/bladekkkkkk.png"
+        githubLink="https://github.com/bladekkkkkk"
+      />
+
+      <Estudante
+        nome="Tainara de Oliveira Dias"
+        descricao="Aluna no IFMS-NV"
+        foto="https://github.com/Tainaradias31.png"
+        githubLink="https://github.com/Tainaradias31"
+      />
+      <Estudante
+        nome="Ana Carla"
+        descricao="Estudante do IFMS-NV"
+        foto="https:/github.com/anacarla2212.png"
+        githubLink="https://github.com/anacarla2212"
+      />
+      <Estudante
+        nome="Pedro Lucas Alves da Silva"
+        descricao="Estudante do IFMS"
+        foto="https://github.com/ETHGWRTJTYJ.png"
+        githubLink="https://github.com/ETHGWRTJTYJ?tab=repositories"
+      />
+      <Estudante
+        nome="Vinícius Amâncio Martins Pereira"
+        descricao="Estudante do IFMS"
+        foto="https://github.com/vinizerahsz.png"
+        githubLink="https://github.com/vinizerahsz"
+      />
+
+      <Estudante
+        nome="Kauan Da Silva"
+        descricao="Aluno no IFMS-NV"
+        foto="https://github.com/kauanSilva01.png"
+        githubLink="https://github.com/kauanSilva01"
+      />
+      <Estudante
+        nome="Emily Marques"
+        descricao="Estudante no IFMS-NV"
+        foto="https:/github.com/ime-m.png"
+        githubLink="https://github.com/ime-m"
+      />
+      <Estudante
+        nome="Júlia Strey Bem"
+        descricao="Estudante do IFMS, turma: 1022-A"
+        foto="https://github.com/J0lix.png"
+        githubLink="https://github.com/J0lix"
+      />
+      <Estudante
+        nome="Lays Wimmer"
+        descricao="Aluna IFMS"
+        foto="https://github.com/layswim.png"
+        githubLink="https://github.com/layswim"
+      />
+      <Estudante
+        nome="Iwarley Silva Santana"
+        descricao="Aluno no IFMS-NV"
+        foto="https://github.com/Iwarley.png"
+        githubLink="https://github.com/iwarley"
+      />
+      <Estudante
+        nome="Vinicius Azevedo Matheus"
+        descricao="Estudante no IFMS-NV"
+        foto="https://github.com/Jubiscreubio.png"
+        githubLink="https://github.com/Jubiscreubio"
+      />
+      <Estudante
+        nome="Bianca de Oliveira Moraes"
+        descricao="Estudante do IFMS"
+        foto="https://github.com/biawo.png"
+        githubLink="https://github.com/biawo"
+      />
+      <Estudante
+        nome="André Angelo"
+        descricao="so Deus sabe como ta a mente do palhaço"
+        foto="https://github.com/GarotoMeninoHack.png"
+        githubLink="https://github.com/GarotoMeninoHack"
+      />
+      <Estudante
+        nome="Gabriel Sales"
+        descricao="Aluno do IFMS"
+        foto="https://github.com/SallesTachiyomi.png"
+        githubLink="https://github.com/SallesTachiyomi"
+      />
+      <Estudante
+        nome="Matheus Gehrke da Silva"
+        descricao="Estudante no IFMS-NV"
+        foto="https://github.com/GehrkeSilva.png"
+        githubLink="https://github.com/GehrkeSilva"
+      />
+    </main>
+  );
 }
 
-export default Main
+export default Main;
